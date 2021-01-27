ab58a640e160e6b58a3e3cd12f8462ba2d50e98a
Sukumar_Banda <sukumarbanda@gmail.com>
2021-01-27 22:38:10 +0530

    Arraylist added

---
+++ src/com/demo/Hello/ArrayList1.java
@@ -0,0 +1,24 @@
+package com.demo.Hello;
+
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+	
+	public static void main(String[] args) {
+		ArrayList<String> obj = new ArrayList<String>();
+		obj.add("sunil");
+		obj.add("Harish");
+		obj.add("Pandey");
+		
+		for (String string : obj) {
+			System.out.println(string);
+			
+			
+		}
+		
+	}
+	
+	
+	
+}
